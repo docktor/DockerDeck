ab3992f51c8417b39971358c0a1fb23926addae9
François SAMIN <francois.samin@gmail.com>
2014-11-19 20:51:40 +0100

    Dev in progress

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
  * Port : tcp docker port, 4243 by default
  * Callback : function (daemonInfo)
  **/
-function addDeamon( host,  port, callBackSuccess, callBackError) {
+function connectDaemon( host,  port, callBackSuccess, callBackError) {
 	console.log("Try to connect to " + host + ":" + port + "...");
 	var daemon = {
 			host : host, 
@@ -13,7 +13,8 @@ function addDeamon( host,  port, callBackSuccess, callBackError) {
 			textStatus : null,
 			error : null,
 			version : null,
-			since : new Date().getTime()
+			since : Math.floor(new Date().getTime() / 1000),
+			runningContainers : []
 		};
 
 	daemon.host = host || '127.0.0.1';
@@ -36,7 +37,7 @@ function addDeamon( host,  port, callBackSuccess, callBackError) {
 		callBackError(daemon);
 	});
 	
-$.ajax({
+	$.ajax({
 		crossDomain: true,
         type:"GET",
 		contentType: "application/json; charset=utf-8",
@@ -53,22 +54,97 @@ $.ajax({
 		callBackError(daemon);
 	});
 
+	setInterval(loadDockerDaemonEvents, 5000);
+	setInterval(loadDockerDaemonLive, 5000);
+}
+
+function getRunningContainers(host,  port, callBackSuccess, callBackError) {
+	var daemon = {
+			host : host, 
+			port : port, 
+			textStatus : null,
+			error : null,
+			runningContainers : []
+		};
+
+	$.ajax({
+		crossDomain: true,
+        type:"GET",
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		url: 'http://' + host + ':' + port + '/containers/json'
+	}).done(function(data,  textStatus, jqXHR) {
+		daemon.runningContainers = data;
+		daemon.textStatus = textStatus;
+		callBackSuccess(daemon);
+	}).fail(function( jqXHR, textStatus, errorThrown) {
+		daemon.runningContainers = null;
+		daemon.textStatus = "Connection to Docker Daemon failed";
+		daemon.error = "Error : unable to connect to " + daemon.host + ":"+ daemon.port;
+		callBackError(daemon);
+	});
+}
+
+function getEvents(host,  port, since, until, callBackSuccess, callBackError) {
+	var url = 'http://' + host + ':' + port + '/events?since=' + since + '&until=' + until;
+	console.log(url);
+
+	if (host !== null && port !== null && since !== null && !until !== null ) {	
+		listEvents = {
+			events: []
+		};
+		$.get(url)
+		.done(
+			function(data,  textStatus, jqXHR) {
+				console.log(data);
+				listEvents.events =  data;
+				console.log(listEvents);
+				callBackSuccess(listEvents);
+			})
+		.fail(function( jqXHR, textStatus, errorThrown) {
+			try {
+				var data = JSON.parse('[' + jqXHR.responseText.replace(/}{/g, '},{') + ']');
+				listEvents.events =  data;
+				console.log(listEvents);
+				callBackSuccess(listEvents);
+			} catch (err) {
+				console.error(err);
+			}
+		});
+	} else {
+		console.log("wait...")
+	}
+}
+
+function renderMustache(idTemplate, idTarget, object) {
+	var template = $("#" + idTemplate).html();
+	var html = Mustache.to_html(template, object);
+	$('#' + idTarget).html(html);
 }
 
 function renderDockerDaemonView(daemon) {
 	emptyDockerDaemonView(daemon.error);
 	if (daemon.info) {
-		var template = $("#dockerDaemonView").html();
-		var html = Mustache.to_html(template, daemon);
-		$('#dockerDaemonViewContainer').html(html);
+		renderMustache('dockerDaemonView', 'dockerDaemonViewContainer', daemon);
 	}
 	if (daemon.version) {
-		var template = $("#dockerDaemonVersion").html();
-		var html = Mustache.to_html(template, daemon);
-		$('#dockerDaemonVersionContainer').html(html);
+		renderMustache('dockerDaemonVersion', 'dockerDaemonVersionContainer', daemon);
 	}
+	renderLive(daemon);
+	renderMustache('dockerDaemonEventsView', 'dockerDaemonEventsContainer', {});
 
 	initControllers();
+}
+
+function renderEvents(listEvents) {
+	renderMustache('dockerDaemonEventsList', 'dockerDaemonEventsListContainer', listEvents);
+}
+
+function renderLive(daemon) {
+	if (daemon.runningContainers) {
+		console.log(daemon.runningContainers);
+		renderMustache('dockerDaemonLive', 'dockerDaemonLiveContainer', daemon);
+	}
 }
 
 function renderDockerDaemonViewError(daemon) {
@@ -80,17 +156,33 @@ function renderDockerDaemonViewError(daemon) {
 }
 
 function loadDockerDaemonEvents() {
-	//http://localhost:4243/events?since=1415727850&until=1415729025
+	var host = $('#dockerDaemonHost').val();
+	var port = $('#dockerDaemonPort').val();
+	var since = $('#dockerDaemonConnectionTime').val();
+	var until = Math.floor(new Date().getTime() / 1000);	
+	getEvents(host, port, since, until, renderEvents, renderDockerDaemonViewError);
+	
+}
+
+function loadDockerDaemonLive() {
+	var host = $('#dockerDaemonHost').val();
+	var port = $('#dockerDaemonPort').val();
+	getRunningContainers(host, port, renderDockerDaemonView, renderDockerDaemonViewError);
+	
 }
 
 function emptyDockerDaemonView(error) {
 	if (error) {
 		$('#dockerDaemonViewContainer').html('');
 		$('#dockerDaemonVersionContainer').html('');
+		$('#dockerDaemonEventsContainer').html('');
+		$('#dockerDaemonLiveContainer').html('');
 		$('#dockerDaemonConnectForm').fadeIn();
 	} else {
 		$('#dockerDaemonViewContainer').html('');
 		$('#dockerDaemonVersionContainer').html('');
+		$('#dockerDaemonEventsContainer').html('');
+		$('#dockerDaemonLiveContainer').html('');
 		$('#dockerDaemonViewErrorContainer').html('');
 		$('#dockerDaemonConnectForm').fadeOut();
 	}	
@@ -98,7 +190,7 @@ function emptyDockerDaemonView(error) {
 }
 
 function connectToDaemon() {
-	addDeamon(
+	connectDaemon(
 		$('#addDockerDeamonHost').val(), 
 		$('#addDockerDeamonPort').val(), 
 		renderDockerDaemonView,
